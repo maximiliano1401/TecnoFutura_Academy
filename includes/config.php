214d17883120f9280ad4db0b2545a87c509ab714
David <alu.2411305@itesrenedescartes.edu.mx>
2026-02-25 16:53:38 -0600

    rutas relativas

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,11 @@
 // Configuración del sitio
 define('SITE_NAME', 'TecnoFutura Academy');
 define('SITE_DESCRIPTION', 'Plataforma de aprendizaje especializada en Arduino, electrónica y programación de bajo nivel');
-define('SITE_URL', 'http://localhost/TecnoFutura_Academy');
+
+// Detectar la URL base dinámicamente
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$host = $_SERVER['HTTP_HOST'];
+define('SITE_URL', $protocol . $host);
 
 // Información de contacto
 define('CONTACT_EMAIL', 'contacto@tecnofutura.academy');
@@ -17,12 +21,12 @@ define('TWITTER_URL', '#');
 define('INSTAGRAM_URL', '#');
 define('LINKEDIN_URL', '#');
 
-// Configuración de rutas
+// Configuración de rutas relativas desde la raíz del sitio
 define('BASE_PATH', __DIR__ . '/..');
-define('CSS_PATH', SITE_URL . '/css');
-define('JS_PATH', SITE_URL . '/js');
-define('IMG_PATH', SITE_URL . '/img');
-define('LMS_URL', SITE_URL . '/lms');
+define('CSS_PATH', '/css');
+define('JS_PATH', '/js');
+define('IMG_PATH', '/img');
+define('LMS_URL', '/lms');
 
 // Zona horaria
 date_default_timezone_set('America/Mexico_City');
